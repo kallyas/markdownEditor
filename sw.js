bce41a1e0414bb73b231ac7ccd0c626d1fba1cbb
Kally <kallyasmedia@gmail.com>
2020-05-21 15:20:10 +0000

    fixed caching

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 if (workbox) {
-	console.log(`Yay! Workbox is loaded 🎉`);
+	console.log(`Yay! Workbox is loaded 🎉😉`);
 } else {
 	console.log(`Boo! Workbox didn't load 😬`);
 }
